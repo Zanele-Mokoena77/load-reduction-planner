bc9dfcd4a39955089758604276b1d26082b765ec
Zanele Mokoena <zanelemokoenatech@gmail.com>
2026-01-03 15:20:08 +0200

    implemented function to calculate the safe window

--- scheduler.py
+++ scheduler.py
@@ -15,4 +15,28 @@ def get_outages(data: dict, suburb: str) -> list:
 
     resp = data.get(suburb, {}).get(date, [])
     return resp
+
+
+def calculate_safe_window(outages: list) -> list:
+
+    safe_windows = []
+    current_start = DAY_START
+
+    for start, end in outages:
+        outage_start = parse_time(start)
+        outage_end = parse_time(end)
+
+        if outage_start > current_start:
+            safe_windows.append([current_start, outage_start])
+
+        current_start = outage_end
+
+    
+    if current_start < DAY_END:
+        safe_windows.append([current_start, DAY_END])
+
+
+    return safe_windows
+
+
         
